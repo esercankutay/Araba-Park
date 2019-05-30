bde817b86deff7d683c190c3e40142be91ebbc1d
Can <email@cankutayeser.com>
2019-05-30 12:41:30 +0300

    Add project files.

---
+++ Araba Park/main.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Araba_Park
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.sp = new System.IO.Ports.SerialPort(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port seçin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bağlı cihaz yok";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(29, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(198, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Bağlan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(121, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Bağlantıyı Kes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Araba_Park.Properties.Resources.IMG_20190529_010518;
+            this.pictureBox1.Location = new System.Drawing.Point(606, 133);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(506, 245);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button6
+            // 
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.Image = global::Araba_Park.Properties.Resources.arrow_right;
+            this.button6.Location = new System.Drawing.Point(213, 426);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 71);
+            this.button6.TabIndex = 8;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Image = global::Araba_Park.Properties.Resources.arrow_down;
+            this.button5.Location = new System.Drawing.Point(121, 426);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 71);
+            this.button5.TabIndex = 7;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Image = global::Araba_Park.Properties.Resources.arrowt_left;
+            this.button4.Location = new System.Drawing.Point(27, 426);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 71);
+            this.button4.TabIndex = 6;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(121, 349);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 71);
+            this.button3.TabIndex = 5;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // sp
+            // 
+            this.sp.BaudRate = 38400;
+            this.sp.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.Sp_DataReceived);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(426, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 73);
+            this.label3.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(1239, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 73);
+            this.label4.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(1129, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(259, 55);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Ön Mesafe";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(292, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(294, 55);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Arka Mesafe";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1294, 21);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Çıkış";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1400, 544);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.Name = "main";
+            this.Text = "Araba Park";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.key_down);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.key_up);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.IO.Ports.SerialPort sp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Araba Park/main.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Araba_Park
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (String item in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(item);
+            }
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.Text = comboBox1.Items[0].ToString();
+            }
+            comboBox1.Enabled = true;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            pictureBox1.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label2.Text = "Bağlı cihaz yok";
+
+        }
+        
+        
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Items.Count == 0)
+            {
+                MessageBox.Show("Bağlanacak cihaz yok!");
+            }
+            else
+            {
+                sp.PortName = comboBox1.Text;
+                try
+                {
+                    sp.Open();
+                    label2.Text = "Bağlandı";
+                    comboBox1.Enabled = false;
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                    button4.Enabled = true;
+                    button5.Enabled = true;
+                    button6.Enabled = true;
+                    pictureBox1.Visible = true;
+                    label3.Visible = true;
+                    label4.Visible = true;
+                    label5.Visible = true;
+                    label6.Visible = true;
+                }
+                catch (System.IO.IOException ex)
+                {
+                    MessageBox.Show("Bağlantı kurulamadı! \nAracın veya Bluetooth bağlantısının açık olduğundan emin olun...");
+                    Console.WriteLine("Bağlantı hatası : " + ex.ToString());
+                    throw ex;
+                }
+            }
+        }
+
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (sp.IsOpen)
+            {
+                sp.Close();
+                label2.Text = "Bağlantı kesildi";
+                
+                comboBox1.Enabled = true;
+                button1.Enabled = true;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+                pictureBox1.Visible = false;
+                label3.Visible = false;
+                label4.Visible = false;
+                label5.Visible = false;
+                label6.Visible = false;
+            }
+        }
+
+        public void key_down(object sender, KeyEventArgs e)
+        {
+            if (sp.IsOpen)
+            {
+                if (e.KeyCode == Keys.W)
+                {
+                    button3.BackColor = Color.Red;
+                    sp.Write("w");
+                }
+                if (e.KeyCode == Keys.A)
+                {
+                    button4.BackColor = Color.Red;
+                    sp.Write("a");
+                }
+                if (e.KeyCode == Keys.D)
+                {
+                    button6.BackColor = Color.Red;
+                    sp.Write("d");
+                }
+                if (e.KeyCode == Keys.S)
+                {
+                    button5.BackColor = Color.Red;
+                    sp.Write("s");
+                }
+            }
+        }
+        public void key_up(object sender, KeyEventArgs e)
+        {
+            button3.BackColor = SystemColors.Control;
+            button4.BackColor = SystemColors.Control;
+            button5.BackColor = SystemColors.Control;
+            button6.BackColor = SystemColors.Control;
+
+            sp.Write("z");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            sp.Close();
+        }
+
+        private void Sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (sp.IsOpen)
+            {
+                String[] mesafe = sp.ReadLine().Split(',');
+                int onMesafe, arkaMesafe;
+                onMesafe = Convert.ToInt32(mesafe[0]);
+                arkaMesafe = Convert.ToInt32(mesafe[1]);
+                if (onMesafe<7)
+                {
+                    label4.Invoke(new Action(() => label4.ForeColor = Color.Red));
+                }
+                else if (onMesafe < 12)
+                {
+                    label4.Invoke(new Action(() => label4.ForeColor = Color.Orange));
+                }
+                else
+                {
+                    label4.Invoke(new Action(() => label4.ForeColor = Color.Green));
+                }
+                if (arkaMesafe < 7)
+                {
+                    label3.Invoke(new Action(() => label3.ForeColor = Color.Red));
+                }
+                else if (arkaMesafe < 12)
+                {
+                    label3.Invoke(new Action(() => label3.ForeColor = Color.Orange));
+                }
+                else
+                {
+                    label3.Invoke(new Action(() => label3.ForeColor = Color.Green));
+                }
+                label3.Invoke(new Action(() => label3.Text = mesafe[1]));
+                label4.Invoke(new Action(() => label4.Text = mesafe[0]));
+                
+
+            }
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
